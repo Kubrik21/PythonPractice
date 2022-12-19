8b646eb7322fe6158f6ea7db991a458f452cae7d
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-19 11:49:39 +0100

    "Edit" feature in GUI

--- Zad1/gui.py
+++ Zad1/gui.py
@@ -5,7 +5,10 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box=sg.InputText(tooltip="Enter todo",key="todo")
 add_button=sg.Button("Add")
-layout=[[label],[input_box,add_button]]
+listBox=sg.Listbox(values=funct.load_todo(),key="listbox",enable_events=True,size=[45,10])
+edit_button=sg.Button("Edit")
+
+layout=[[label],[input_box,add_button],[listBox,edit_button]]
 
 window = sg.Window("Todo App",layout=layout,
                    font=('Helvetica',16),
@@ -19,8 +22,23 @@ while True:
         case "Add":
             todos=funct.load_todo()
             new_todo=values["todo"]
-            todos.append(new_todo)
+            todos.append(f"{new_todo}\n")
             funct.save_todo(todos)
+
+            window["listbox"].update(values=todos)
+        case "Edit":
+            todo=values["listbox"][0]
+            new_value=values["todo"]
+
+
+            load=funct.load_todo()
+            index = load.index(todo)
+            load[index] = f"{new_value}\n"
+            funct.save_todo(load)
+
+            window["listbox"].update(values=load)
+        case "listbox":
+            window["todo"].update(value=values["listbox"][0])
         case sg.WIN_CLOSED:
             break
 
