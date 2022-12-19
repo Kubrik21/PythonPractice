3cba8750399e583f32b18b63e50b31ff791114f5
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-19 11:21:12 +0100

    "Add" feature in GUI

--- Zad1/funct.py
+++ Zad1/funct.py
@@ -1,9 +1,9 @@
-
-def load_todo(filepath):
+FILEPATH = "todos.txt"
+def load_todo(filepath=FILEPATH):
     with open(filepath, 'r') as file:
         list = file.readlines()
     return list
 
-def save_todo(filepath,content):
+def save_todo(content,filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(content)
--- Zad1/gui.py
+++ Zad1/gui.py
@@ -1,11 +1,27 @@
 import funct
+#import cli
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box=sg.InputText(tooltip="Enter todo")
+input_box=sg.InputText(tooltip="Enter todo",key="todo")
 add_button=sg.Button("Add")
 layout=[[label],[input_box,add_button]]
 
-window = sg.Window("Todo App",layout=layout)
-window.read()
+window = sg.Window("Todo App",layout=layout,
+                   font=('Helvetica',16),
+                   size=(700,500)
+                   )
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=funct.load_todo()
+            new_todo=values["todo"]
+            todos.append(new_todo)
+            funct.save_todo(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
