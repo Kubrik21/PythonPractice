fca278f8c1d66732fc2c7acfded9044e8fc1f103
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2022-12-20 11:18:59 +0100

    Complete tasks WebApp

--- Zad1/web.py
+++ Zad1/web.py
@@ -7,6 +7,15 @@ def add_todo():
     print(todos)
     funct.save_todo(todos,r"C:\Users\olejn\Desktop\PythonCourse40Days\1Project\Zad1\todos.txt")
 
+def complete_task():
+    for elem in todos:
+        if st.session_state[elem]==True:
+            todos.remove(elem)
+            del st.session_state[elem]
+
+    funct.save_todo(todos, r"C:\Users\olejn\Desktop\PythonCourse40Days\1Project\Zad1\todos.txt")
+
+
 todos=funct.load_todo(r"C:\Users\olejn\Desktop\PythonCourse40Days\1Project\Zad1\todos.txt")
 
 st.title("My todo App")
@@ -14,8 +23,11 @@ st.subheader("This is my todo app.")
 
 
 
-for index,todo in enumerate(todos):
-    st.checkbox(todo,key=index)
-    print(index,": ",todo)
+for todo in todos:
+    st.checkbox(todo,key=todo, on_change=complete_task)
+    #print(index,": ",todo)
 
 st.text_input(label="Emter a todo",placeholder="Add a new todo...",on_change=add_todo,key='new_todo')
+
+
+st.session_state
